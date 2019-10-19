f8ec7a8199e1c6716d5cea3a23073f73159323ab
Carlos Alberto Cabrera <carlcaba@gmail.com>
2019-10-19 17:13:56 -0500

    First attempt to solve the challenge
    WARNING: It must be a little slow. There is over 1.000.000 iterations with the 1000x1000 matrix.

---
+++ ExperisChallenge/Position.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperisChallenge
+{
+    /// <summary>
+    /// Class to determine the weight between two positions
+    /// </summary>
+    public class Weight
+    {
+        /// <summary>
+        /// Value of weight
+        /// </summary>
+        public int value { get; set; }
+        /// <summary>
+        /// Direction (North, West, South, East)
+        /// </summary>
+        public string direction { get; set; }
+        /// <summary>
+        /// Index of the column (zero-based)
+        /// </summary>
+        public int column { get; set; }
+        /// <summary>
+        /// Index of the row (zero-based)
+        /// </summary>
+        public int row { get; set; }
+    }
+
+    /// <summary>
+    /// Class for any position on matrix
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// Index of the column (zero-based)
+        /// </summary>
+        public int column { get; set; }
+        /// <summary>
+        /// Index of the row (zero-based)
+        /// </summary>
+        public int row { get; set; }
+        /// <summary>
+        /// Value of position
+        /// </summary>
+        public int value { get; set; }
+        /// <summary>
+        /// List of all weights
+        /// </summary>
+        public List<Weight> weight { get; set; }
+        /// <summary>
+        /// Property to determine if all weight has been already calculated
+        /// </summary>
+        public bool GetAllWeights
+        {
+            get
+            {
+                return this.weight.Count() == 0;
+            }
+        }
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public Position()
+        {
+            //Assign weights by default
+            this.weight = new List<Weight>();
+        }
+    }
+    /// <summary>
+    /// Class to get all posible paths
+    /// </summary>
+    public class Results
+    {
+        /// <summary>
+        /// Trace of every position
+        /// </summary>
+        public List<Position> trace { get; set; }
+        /// <summary>
+        /// Property to count steps
+        /// </summary>
+        public int Steps
+        {
+            get
+            {
+                return this.trace.Count();
+            }
+        }
+
+        public int Drop
+        {
+            get
+            {
+                int result = 0;
+                if (this.trace != null && this.trace.Count() > 0)
+                {
+                    result = this.trace.Max(x => x.value) - this.trace.Min(x => x.value);
+                }
+                return result;
+            }
+        }
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public Results()
+        {
+            this.trace = new List<Position>();
+        }
+    }
+
+    /// <summary>
+    /// Extension to list of class Position
+    /// </summary>
+    public static class PositionExtension
+    {
+        /// <summary>
+        /// Get the North Limit and weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <param name="item">Position to analize</param>
+        public static void GetNorthWeight(this List<Position> list, Position item)
+        {
+            int? result = null;
+            //Add north
+            if (item.row > 0)
+            {
+                Position limit_north = list.Where(x => x.row == (item.row - 1) && x.column == item.column).FirstOrDefault();
+                result = (limit_north != null ? (item.value - limit_north.value) : result);
+            }
+            //Gets only numbers > 0
+            if (result != null && result > 0) {
+                item.weight.Add(new Weight { direction = "N", value = result.Value, row = item.row - 1, column = item.column });
+            }
+        }
+        /// <summary>
+        /// Get the South Limit and weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <param name="item">Position to analize</param>
+        public static void GetSouthWeight(this List<Position> list, Position item, int max_row)
+        {
+            int? result = null;
+            //Add south
+            if (item.row < max_row)
+            {
+                Position limit_south = list.Where(x => x.row == (item.row + 1) && x.column == item.column).FirstOrDefault();
+                result = (limit_south != null ? item.value - limit_south.value : result);
+            }
+            //Gets only numbers > 0
+            if (result != null && result > 0)
+            {
+                item.weight.Add(new Weight { direction = "S", value = result.Value, row = item.row + 1, column = item.column });
+            }
+        }
+        /// <summary>
+        /// Get the West Limit and weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <param name="item">Position to analize</param>
+        public static void GetWestWeight(this List<Position> list, Position item)
+        {
+            int? result = null;
+            //Add west
+            if (item.column > 0)
+            {
+                Position limit_west = list.Where(x => x.row == item.row && x.column == (item.column - 1)).FirstOrDefault();
+                result = (limit_west != null ? item.value - limit_west.value : result);
+            }
+            //Gets only numbers > 0
+            if (result != null && result > 0)
+            {
+                item.weight.Add(new Weight { direction = "W", value = result.Value, row = item.row, column = item.column - 1 });
+            }
+        }
+        /// <summary>
+        /// Get the East Limit and weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <param name="item">Position to analize</param>
+        public static void GetEastWeight(this List<Position> list, Position item, int max_col)
+        {
+            int? result = null;
+            //Add east
+            if (item.column < max_col)
+            {
+                Position limit_east = list.Where(x => x.row == item.row && x.column == (item.column + 1)).FirstOrDefault();
+                result = (limit_east != null ? item.value - limit_east.value : result);
+            }
+            //Gets only numbers > 0
+            if (result != null && result > 0)
+            {
+                item.weight.Add(new Weight { direction = "E", value = result.Value, row = item.row, column = item.column + 1 });
+            }
+        }
+        public static void AddStep(this List<Position> list, Position initial, Weight lead, string[] max, ref Results result, bool starter = false)
+        {
+            //Gets all weights, if not defined
+            if (initial.GetAllWeights)
+            {
+                list.GetNorthWeight(initial);
+                list.GetWestWeight(initial);
+                list.GetSouthWeight(initial, Int16.Parse(max[0]));
+                list.GetEastWeight(initial, Int16.Parse(max[1]));
+            }
+            //Add Position to result
+            result.trace.Add(initial);
+            //Variable for direction
+            Weight direction = null;
+            //Determine the direction
+            if (starter)
+            {
+                direction = lead;
+            }
+            else
+            {
+                direction = initial.weight.GetMin();
+            }
+            //If there is no direction to lead
+            if (direction != null)
+            {
+                //Detemine the next Position
+                Position next = list.Where(n => n.row == direction.row && n.column == direction.column).FirstOrDefault();
+                //If no path to lead
+                if (next != null)
+                {
+                    //Calls recursively
+                    list.AddStep(next, direction, max, ref result, false);
+                }
+            }
+        }
+        public static List<Position> NextSteps(this List<Position> list, Position item)
+        {
+            List<Position> result = new List<Position>();
+            List<Weight> steps = item.weight.OrderBy(x => x.value).ToList();
+            foreach (Weight wgt in steps)
+            {
+                int row = item.row;
+                int col = item.column;
+                if (wgt.direction == "N")
+                {
+                    row--;
+                }
+                else if(wgt.direction == "S")
+                {
+                    row++;
+                }
+                else if(wgt.direction == "E")
+                {
+                    col++;
+                }
+                else if (wgt.direction == "W")
+                {
+                    col--;
+                }
+                Position pos = list.Where(x => x.row == row && x.column == col).FirstOrDefault();
+                if (pos != null)
+                {
+                    result.Add(pos);
+                }
+            }
+            return result; 
+        }
+    }
+
+    public static class WeightExtension
+    {
+        /// <summary>
+        /// Get the minimum weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <returns>Minimum weight object or null</returns>
+        public static Weight GetMin(this List<Weight> list)
+        {
+            Weight result = null;
+            foreach (Weight wght in list)
+            {
+                if (result == null)
+                {
+                    result = new Weight
+                    {
+                        column = wght.column,
+                        direction = wght.direction,
+                        row = wght.row,
+                        value = wght.value
+                    };
+                }
+                else
+                {
+                    if (wght.value < result.value)
+                    {
+                        result = new Weight
+                        {
+                            column = wght.column,
+                            direction = wght.direction,
+                            row = wght.row,
+                            value = wght.value
+                        };
+                    }
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Get the maximum weight
+        /// </summary>
+        /// <param name="list">Reference to object</param>
+        /// <returns>Maximum weight object or null</returns>
+        public static Weight GetMax(this List<Weight> list)
+        {
+            Weight result = null;
+            foreach (Weight wght in list)
+            {
+                if (result == null)
+                {
+                    result = new Weight
+                    {
+                        column = wght.column,
+                        direction = wght.direction,
+                        row = wght.row,
+                        value = wght.value
+                    };
+                }
+                else
+                {
+                    if (wght.value > result.value)
+                    {
+                        result = new Weight
+                        {
+                            column = wght.column,
+                            direction = wght.direction,
+                            row = wght.row,
+                            value = wght.value
+                        };
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ExperisChallenge/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperisChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Results> Result = new List<Results>();
+            List<Position> Matrix = new List<Position>();
+            string textFile = "map.txt";
+            string[] max = new string[2];
+            if (args.Length > 1)
+            {
+                textFile = args[0];
+            }
+            if (string.IsNullOrEmpty(textFile))
+            {
+                Console.WriteLine("No data to analize");
+                Console.ReadKey();
+                return;
+            }
+            //If file exists
+            if (File.Exists(textFile))
+            {
+                // Read all lines 
+                string[] lines = File.ReadAllLines(textFile);
+                //Get the maxtrix dimension
+                max = lines[0].Split(' ');
+                //Delete first line (matrix dimension)
+                lines = lines.Skip(1).ToArray();
+                //Define counter
+                int counter = 0;
+                //For every line found
+                foreach (string line in lines)
+                {
+                    int column = 0;
+                    string[] data = line.Split(' ');
+                    //Create the Position list
+                    foreach (string value in data)
+                    {
+                        Position pos = new Position
+                        {
+                            column = column++,
+                            row = counter,
+                            value = Int32.Parse(value)
+                        };
+                        Matrix.Add(pos);
+                    }
+                    counter++;
+                }
+            }
+
+            //If no data on Matrix
+            if (Matrix.Count == 0)
+            {
+                Console.WriteLine("No data on matrix to analize");
+                Console.ReadKey();
+                return;
+            }
+            //Order the matrix for starter Position
+            List<Position> OrderedMatrix = Matrix.OrderByDescending(m => m.value).ToList();
+            foreach (Position pos in OrderedMatrix)
+            {
+                //Gets all weights, if not defined
+                if (pos.GetAllWeights)
+                {
+                    OrderedMatrix.GetNorthWeight(pos);
+                    OrderedMatrix.GetWestWeight(pos);
+                    OrderedMatrix.GetSouthWeight(pos, Int16.Parse(max[0]));
+                    OrderedMatrix.GetEastWeight(pos, Int16.Parse(max[1]));
+                }
+                //Check every weight
+                foreach (Weight weight in pos.weight.OrderBy(w => w.value))
+                {
+                    Results path = new Results();
+                    OrderedMatrix.AddStep(pos, weight, max, ref path, true);
+                    Result.Add(path);
+                }
+            }
+
+            //Print the results
+            Results FinalResult = Result.OrderByDescending(r => r.Drop).ThenByDescending(r => r.Steps).FirstOrDefault();
+            Console.WriteLine(string.Join(" => ", FinalResult.trace.Select(x => x.value.ToString()).ToArray()));
+            Console.WriteLine("Steps: " + FinalResult.Steps);
+            Console.WriteLine("Drop: " + FinalResult.Drop);
+            Console.WriteLine("Press a key to continue...");
+            Console.ReadLine();
+        }
+
+
+    }
+}
